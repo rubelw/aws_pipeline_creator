265513ae78427ad71ac049419339be73c41a4697
Will Rubel <rubelwi@Wills-MacBook-Pro.local>
2018-09-01 12:30:43 -0500

    Fix readme

--- README.rst
+++ README.rst
@@ -1,6 +1,6 @@
 
 AWS Codepipeline Creator
-==================
+========================
 
 Features
 ========
--- aws_pipeline_creator/command.py
+++ aws_pipeline_creator/command.py
@@ -21,7 +21,7 @@ def lineno():
 
 
 @click.group()
-@click.version_option(version='0.0.1')
+@click.version_option(version='0.0.2')
 def cli():
     pass
 
